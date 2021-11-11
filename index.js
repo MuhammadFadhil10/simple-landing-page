f0afcf7e9dce5149987a337cc3522d000f38ac1b
MuhammadFadhil10 <muhammadfadhilakbar10@gmail.com>
2021-11-11 13:07:50 +0000

    menambah fungsi untuk perubahan tombol navigation mobile, agar seperti semula ketika diklik

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // memunculkan navigasi di mobile ketika navigasi diklik
 const mobileNavButton = document.querySelector('.nav-mobile');
+const firstStripe = document.querySelector('.stripe-1');
+const secondStripe = document.querySelector('.stripe-2');
+const thirdStripe = document.querySelector('.stripe-3');
+
 mobileNavButton.addEventListener('click', openNavMobile);
-alert('haha')
+// event binding untuk close navigation 
+document.addEventListener('click', function (e) {
+    console.log(e.target);
+    if(e.target.classList.contains('close-nav-mobile')) {
+        // console.log(this);
+        // e.target.addEventListener('click', closeNavMobile)
+        const closeNavButton = document.querySelector('.close-nav-mobile');
+        closeNavButton.addEventListener('click', closeNavMobile)
+        // closeNavMobile()
+        mobileNavButton.removeEventListener('click', openNavMobile)
+    }
+});
+
+function closeNavMobile() {
+    alert('haha')
+    secondStripe.style.backgroundColor = 'red';
+}
 
 function openNavMobile() {
-    const firstStripe = document.querySelector('.stripe-1');
-    const secondStripe = document.querySelector('.stripe-2');
-    const thirdStripe = document.querySelector('.stripe-3');
-    this.classList.toggle('close-nav-mobile')
     console.log(this);
     firstStripe.style.opacity = '0';
     secondStripe.style.transform = 'rotate(-50deg)';
@@ -15,5 +31,6 @@ function openNavMobile() {
     secondStripe.style.height = '60px';
     thirdStripe.style.transform = 'rotate(50deg)';
     thirdStripe.style.height = '60px';
-    // this.firstElementChild.style.display = 'none';
+    mobileNavButton.classList.add('close-nav-mobile');
+    // openNavMobile.remove
 }
