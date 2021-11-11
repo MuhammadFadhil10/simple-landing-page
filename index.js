29d568d8120176a4a73e7007e383234000e74eec
MuhammadFadhil10 <muhammadfadhilakbar10@gmail.com>
2021-11-11 12:28:10 +0000

    membuat variable untuk setiap bagian stripe dari tombol navigation mobile

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // memunculkan navigasi di mobile ketika navigasi diklik
 const mobileNavButton = document.querySelector('.nav-mobile');
 mobileNavButton.addEventListener('click', openNavMobile);
+alert('haha')
 
 function openNavMobile() {
+    const firstStripe = document.querySelector('.stripe-1');
+    const secondStripe = document.querySelector('.stripe-2');
     const thirdStripe = document.querySelector('.stripe-3');
     this.classList.toggle('close-nav-mobile')
     console.log(this);
-    this.firstElementChild.style.opacity = '0';
-    this.firstElementChild.nextElementSibling.style.transform = 'rotate(-50deg)';
-    this.firstElementChild.nextElementSibling.style.marginTop = '1.2rem';
-    this.firstElementChild.nextElementSibling.style.height = '60px';
+    firstStripe.style.opacity = '0';
+    secondStripe.style.transform = 'rotate(-50deg)';
+    secondStripe.style.marginTop = '1.2rem';
+    secondStripe.style.height = '60px';
     thirdStripe.style.transform = 'rotate(50deg)';
     thirdStripe.style.height = '60px';
     // this.firstElementChild.style.display = 'none';
