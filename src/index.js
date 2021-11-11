02018085eeb16f72110ab77af3fa81b0114cb253
MuhammadFadhil10 <muhammadfadhilakbar10@gmail.com>
2021-11-11 10:50:24 +0000

    menghapus alert di line 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-alert('haha')
 // memunculkan navigasi di mobile ketika navigasi diklik
 const mobileNavButton = document.querySelector('.nav-mobile');
 mobileNavButton.addEventListener('click', openNavMobile);
@@ -12,10 +11,8 @@ function openNavMobile() {
     this.firstElementChild.nextElementSibling.style.marginTop = '1.2rem';
     this.firstElementChild.nextElementSibling.style.height = '30px';
     thirdStripe.style.marginTop = '1px';
-    setTimeout(function () {
-        thirdStripe.style.marginBottom = '3px';
-        thirdStripe.style.transform = 'rotate(50deg)';
-        thirdStripe.style.height = '30px';
-    },1000)
+    thirdStripe.style.marginBottom = '3px';
+    thirdStripe.style.transform = 'rotate(50deg)';
+    thirdStripe.style.height = '30px';
     // this.firstElementChild.style.display = 'none';
 }
