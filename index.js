e519727b285ae7aae21482451287ccd3f5f9f607
MuhammadFadhil10 <muhammadfadhilakbar10@gmail.com>
2021-11-11 13:48:40 +0000

    ahh shit, can i fvcking do this, dhil?

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 // memunculkan navigasi di mobile ketika navigasi diklik
 const mobileNavButton = document.querySelector('.nav-mobile');
+const closeNavButton = document.querySelector('.close-nav-mobile');
 const firstStripe = document.querySelector('.stripe-1');
 const secondStripe = document.querySelector('.stripe-2');
 const thirdStripe = document.querySelector('.stripe-3');
 
 mobileNavButton.addEventListener('click', openNavMobile);
-const closeNavButton = document.querySelector('.close-nav-mobile');
 // event binding untuk close navigation 
-document.addEventListener('click', function (e) {
-    if(e.target.classList.contains === 'close-nav-mobile') {
-        this.addEventListener('click', closeNavMobile)
-    }
-});
+closeNavButton.addEventListener('click', closeNavMobile)
 
 function closeNavMobile() {
-    console.log('tutup');
+    console.log('haha');
+    this.style.display = 'none';
+    mobileNavButton.style.display = 'block';
 }
 
 function openNavMobile() {
-   console.log('buka');
+   this.style.display = 'none';
+   closeNavButton.style.display = 'block';
 }
