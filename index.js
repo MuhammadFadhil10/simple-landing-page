22677fdea20a79d36bf34295641a1d5b4c9f424d
MuhammadFadhil10 <muhammadfadhilakbar10@gmail.com>
2021-11-11 16:24:10 +0000

    memunculkan navigasi mobile beserta backgroundnya ketika tombol navigasi mobile diklik

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const closeNavButton = document.querySelector('.close-nav-mobile');
 const firstStripe = document.querySelector('.stripe-1');
 const secondStripe = document.querySelector('.stripe-2');
 const thirdStripe = document.querySelector('.stripe-3');
+const bgNavMobile = document.querySelector('.nav-mobile-background');
+const navigation = document.querySelector('.navigation');
 
 mobileNavButton.addEventListener('click', openNavMobile);
 // event binding untuk close navigation 
@@ -13,9 +15,15 @@ function closeNavMobile() {
     console.log('haha');
     this.style.display = 'none';
     mobileNavButton.style.display = 'flex';
+    // bg nav dan nav hilang
+    bgNavMobile.style.display = 'none';
+    navigation.style.display = 'none';
 }
 
 function openNavMobile() {
-   this.style.display = 'none';
-   closeNavButton.style.display = 'block';
+    this.style.display = 'none';
+    closeNavButton.style.display = 'block';
+    // bg nav dan nav muncul
+    bgNavMobile.style.display = 'block';
+    navigation.style.display = 'flex';
 }
